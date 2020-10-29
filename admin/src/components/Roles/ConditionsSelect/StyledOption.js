11543b0fcb257feb00d912464e7f5f2bd634f9fc
vicesalles <vicenc.salles@gmail.com>
2020-10-29 10:33:04 +0100

    interface customization

---
+++ admin/src/components/Roles/ConditionsSelect/StyledOption.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Option } from '@buffetjs/core';
+
+const StyledOption = styled(Option)`
+  > span {
+    display: block !important;
+    color: #3DAE2B !important;
+  }
+`;
+
+export default StyledOption;
